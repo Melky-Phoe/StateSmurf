7be004a1786c77b94e3432b1edb008a1e34a2c32
melkus <josefmelkus@seznam.cz>
2022-02-11 14:42:37 +0100

    documentation

--- SmurfEvaluator/include/state_smurf/log_evaluator/CircuitFinder.hpp
+++ SmurfEvaluator/include/state_smurf/log_evaluator/CircuitFinder.hpp
@@ -5,31 +5,106 @@
 
 #include <stack>
 
-/// Algorithms for finding circuits in SmurfDiagram based on Johnson's algorithm
 namespace state_smurf::log_evaluator {
 
+/**
+ * Class for discovering circuits in state diagrams defined by DiagramSmurf.
+ * Circuit is repeatable set of states.
+ * circuits are used by Evaluator to aggregate states transition into circuits transition
+ *
+ * Class uses Johnson's algorithm methods.
+ * TODO odkaz na johnsona
+ */
 class CircuitFinder {
 public:
+	/**
+	 * Constructor
+	 * @param stateDiagram to search circuits in
+	 */
 	explicit CircuitFinder(diagram::StateDiagram stateDiagram);
 	~CircuitFinder() { free(blocked); blocked = nullptr;};
+	
+	/**
+	 * Find all elementary non-duplicit circuits in state diagram
+	 * @return vector of string-vectors, each representing one circuit
+	 */
 	std::vector<std::vector<std::string>> find();
 
 private:
+	/**
+	 * Unblock vertex and recursively all vertexes that are in blockMap for given vertex
+	 * Vertexes are unblocked when circuit containing them is found
+	 * @param vertex
+	 */
     void unblock(const std::shared_ptr<diagram::Vertex>& vertex);
 
+	/**
+	 * Checks given vertex's adjacent Vertexes, if any is startOfCircuit (bottom on stack), returns true
+	 * recursively calls for all adjacent Vertexes
+	 * @param vertex
+	 * @return true if circuit found
+	 */
     bool circuit(const std::shared_ptr<diagram::Vertex>& vertex);
 
+	/**
+	 * Returns vertex's index in given array (vector)
+	 * @param vertexVector array where to look for vertex
+	 * @param vertex that's index is wanted
+	 * @return index
+	 */
     static long getVertexIndex(const std::vector<std::shared_ptr<diagram::Vertex>>& vertexVector, const std::shared_ptr<diagram::Vertex>& vertex);
 	
+	/**
+	 * Adjacency table, set of possible destination vertexes for each vertex
+	 * every vertex in StateDiagram has its own key and set of destination vertexes
+	 * TODO odkaz
+	 */
     std::map<std::shared_ptr<diagram::Vertex>, std::vector<std::shared_ptr<diagram::Vertex>>> adjacencyMap;
+	
+	/**
+	 * Vector of all vertexes ordered, used for indexing in other vectors
+	 */
     std::vector<std::shared_ptr<diagram::Vertex>> keys;
+	
+	/**
+	 * List of vertexes that will be unblocked if a vertex is unblocked
+	 * For more info check Johnson's algorithm principle
+	 */
     std::vector<std::vector<std::shared_ptr<diagram::Vertex>>> blockMap;
-    std::vector<std::shared_ptr<diagram::Vertex>> stack;
+	
+	/**
+	 * Vector of visited vertexes by circuit()
+	 * has stack-like behavior, but has to be iterated-over, therefore is implemented as vector
+	 * If circuit() finds startVertex, all vertexes in vector are part of a new 4circuit
+	 */
+    std::vector<std::shared_ptr<diagram::Vertex>> visitedVertexes;
+	
+	/**
+	 * total vertexes in state diagram
+	 */
     ulong numberOfVertexes;
+	
+	/**
+	 * list of all startingVertexes of state diagram
+	 * find() starts searching for cycles vertexes of this vector
+	 * after every circuit is found, adjacent vertexes are appended
+	 */
 	std::vector<std::shared_ptr<diagram::Vertex>> startingVertexes;
+	
+	/**
+	 * Vertex that circuit() was originally called on, serve for comparing to find end of circuit
+	 */
     std::shared_ptr<diagram::Vertex> startVertex;
+	
+	/**
+	 * Array indicating if Vertex on given index is blocked.
+	 * Vertexes are blocked if they were already visited and They are unblocked when circuit is found
+	 */
     bool *blocked;
 	
+	/**
+	 * Vector of circuits found. Is returned by find() when all circuits were found
+	 */
 	std::vector<std::vector<std::string>> circuits;
 };
 
--- SmurfEvaluator/source/state_smurf/log_evaluator/CircuitFinder.cpp
+++ SmurfEvaluator/source/state_smurf/log_evaluator/CircuitFinder.cpp
@@ -29,6 +29,8 @@ namespace state_smurf::log_evaluator {
 				for (const auto& adjacentVertex : adjacencyMap[startVertex]) {
 					startingVertexes.push_back(adjacentVertex);
 				}
+				
+				// removing Vertex that has been completly discovered
 				adjacencyMap.erase(startVertex);
 				for (auto & it : adjacencyMap) {
 					auto it2 = std::find(it.second.begin(), it.second.end(), startVertex);
@@ -69,13 +71,13 @@ namespace state_smurf::log_evaluator {
     bool CircuitFinder::circuit(const std::shared_ptr<diagram::Vertex>& vertex) {
         bool found = false;
         long currVertexIndex = getVertexIndex(keys, vertex);
-        stack.push_back(vertex);
+        visitedVertexes.push_back(vertex);
         blocked[currVertexIndex] = true;
 
         for (const auto& nextVertex : adjacencyMap[vertex]) {
             if (nextVertex == startVertex) {
 				std::vector<std::string> newCircuit;
-                for (const auto& circuitVertex : stack) {
+                for (const auto& circuitVertex : visitedVertexes) {
                     // save circuit
                     //std::cout << circuitVertex->getName() << " ";
 					newCircuit.push_back(circuitVertex->getName());
@@ -99,121 +101,7 @@ namespace state_smurf::log_evaluator {
 				}
             }
         }
-		stack.pop_back();
+		visitedVertexes.pop_back();
         return found;
     }
 }
-
-/*
- Johnson v Jave
-public final class CircuitFinding {
-
-    final Stack<Integer> stack = new Stack<Integer>();
-    final Map<Integer, List<Integer>> a;
-    final List<List<Integer>> b;
-    final boolean[] blocked;
-    final int n;
-    Integer s;
-
-public static void main(String[] args) {
-        List<List<Integer>> a = new ArrayList<List<Integer>>();
-        a.add(new ArrayList<Integer>(Arrays.asList(1, 2)));
-        a.add(new ArrayList<Integer>(Arrays.asList(0, 2)));
-        a.add(new ArrayList<Integer>(Arrays.asList(0, 1)));
-        CircuitFinding cf = new CircuitFinding(a);
-        cf.find();
-    }
-
-    **
-     * @param a adjacency structure of strong component K with least vertex in
-     * subgraph of G induced by {s, s + 1, n};
-     *
-public CircuitFinding(List<List<Integer>> A) {
-        this.a = new HashMap<Integer, List<Integer>>(A.size());
-        for (int i = 0; i < A.size(); i++) {
-            this.a.put(i, new ArrayList<Integer>());
-            for (int j : A.get(i)) {
-                this.a.get(i).add(j);
-            }
-        }
-        // toto by mela byt ta adjacency kterou uz mam
-        n = a.size();
-        blocked = new boolean[n];
-        b = new ArrayList<List<Integer>>();
-        for (int i = 0; i < n; i++) {
-            b.add(new ArrayList<Integer>());
-        }
-    }
-
-private void unblock(int u) {
-        blocked[u] = false;
-        List<Integer> list = b.get(u);
-        for (int w : list) {
-            //delete w from B(u);
-            list.remove(Integer.valueOf(w));
-            if (blocked[w]) {
-                unblock(w);
-            }
-        }
-    }
-
-private boolean circuit(int v) {
-        boolean f = false;
-        stack.push(v);
-        blocked[v] = true;
-        L1:
-        for (int w : a.get(v)) {
-            if (w == s) {
-                //output circuit composed of stack followed by s;
-                for (int i : stack) {
-                    System.out.print(i + " ");
-                }
-                System.out.println(s);
-                f = true;
-            } else if (!blocked[w]) {
-                if (circuit(w)) {
-                    f = true;
-                }
-            }
-        }
-        L2:
-        if (f) {
-            unblock(v);
-        } else {
-            for (int w : a.get(v)) {
-                //if (v∉B(w)) put v on B(w);
-                if (!b.get(w).contains(v)) {
-                    b.get(w).add(v);
-                }
-            }
-        }
-        v = stack.pop();
-        return f;
-    }
-
-public void find() {
-        s = 0;
-        while (s < n) {
-            if (!a.isEmpty()) {
-                //s := least vertex in V;
-                L3:
-                for (int i : a.keySet()) {
-                    b.get(i).clear();
-                    blocked[i] = false;
-                }
-                circuit(s);
-                a.remove(s);
-                for (Integer j : a.keySet()) {
-                    if (a.get(j).contains(s)) {
-                        a.get(j).remove(s);
-                    }
-                }
-                s++;
-            } else {
-                s = n;
-            }
-        }
-    }
-}
-
- */
