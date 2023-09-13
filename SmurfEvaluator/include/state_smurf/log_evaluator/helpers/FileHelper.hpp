0e45d0aeb915b41155c5cd2ea13d888dfa0fdbe7
melkus <josefmelkus@seznam.cz>
2023-09-13 11:48:08 +0200

    documentation

--- SmurfEvaluator/include/state_smurf/log_evaluator/helpers/FileHelper.hpp
+++ SmurfEvaluator/include/state_smurf/log_evaluator/helpers/FileHelper.hpp
@@ -8,8 +8,7 @@ namespace state_smurf::log_evaluator::helpers {
 	class FileHelper {
 	public:
 		/**
-		 * Creates directories in path, where aggregated files will be created.
-		 * etalonLogs are written to file 'etalon' and comparedLogs to file 'compared'
+		 * Creates directories in path, where aggregated files will be created. Then create file and fill it with aggregatedLogs
 		 * @param aggregatedLogs vector of aggregated logs
 		 * @param fileName file name
 		 * @param path directory, where the file fileName will be created
@@ -17,12 +16,25 @@ namespace state_smurf::log_evaluator::helpers {
 		 */
 		static bool createAggregatedFiles(const std::vector<std::string>& aggregatedLogs, const std::string &fileName, const std::string& path);
 
-
+		/**
+		 * @brief Puts all logs into file
+		 * @param sourceLogs logs that will be saved
+		 * @param outputStream output file stream
+		 */
 		static void writeVectorToFile(const std::vector<std::string>& sourceLogs, std::ofstream* outputStream);
 
-
-		static bool isFileAggregated(std::istream &file);
-
+		/**
+		 * @brief Reads whole file and puts each line into vector
+		 * @param file input stream
+		 * @return vector of lines
+		 */
 		static std::vector<std::string> createVectorFromFile(std::istream &file);
+
+		/**
+		 * @brief Check if a file is already aggregated.
+		 * @param file
+		 * @return true if aggregated, false otherwise
+		 */
+		static bool isFileAggregated(std::istream &file);
 	};
 }
